da25cc0f8270cb24b7e9ad14aad6e5fe594772e8
danndunk <dandunkariyon1904@gmail.com>
2022-02-16 18:54:32 +0700

    auth

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // Pemanggilan package express
 const express = require("express");
 
+// Import package bcrypt
+const bcrypt = require("bcrypt");
+
+// import package express-flash and express-session
+const flash = require("express-flash");
+const session = require("express-session");
+
 // import db connection
 const db = require("./connection/db");
 
@@ -13,7 +20,23 @@ app.set("view engine", "hbs");
 app.use("/public", express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: false }));
 
-const isLogin = true;
+// use express-flash
+app.use(flash());
+
+// setup session midleware
+app.use(
+  session({
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 2,
+      secure: false,
+      httpOnly: true,
+    },
+    store: new session.MemoryStore(),
+    saveUninitialized: true,
+    resave: false,
+    secret: "secretValue",
+  })
+);
 
 let month = [
   "January",
@@ -36,7 +59,10 @@ app.get("/", function (req, res) {
 });
 
 app.get("/home", function (req, res) {
-  res.render("index");
+  res.render("index", {
+    isLogin: req.session.isLogin,
+    user: req.session.user,
+  });
 });
 
 app.get("/blog", function (req, res) {
@@ -56,17 +82,21 @@ app.get("/blog", function (req, res) {
           ...blog,
           post_at: getFullTime(blog.post_at),
           post_age: getDistanceTime(blog.post_at),
-          isLogin: isLogin,
+          isLogin: req.session.isLogin,
         };
       });
 
-      res.render("blog", { isLogin: isLogin, blogs: data });
+      res.render("blog", {
+        isLogin: req.session.isLogin,
+        user: req.session.user,
+        blogs: data,
+      });
     });
   });
 });
 
 app.get("/add-blog", function (req, res) {
-  if (!isLogin) {
+  if (!req.session.isLogin) {
     res.redirect("/home");
   }
 
@@ -174,6 +204,75 @@ app.post("/update-blog/:id", function (req, res) {
 
 app.get("/contact-me", function (req, res) {
   res.render("contact");
+});
+
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+app.post("/register", function (req, res) {
+  let { name, email, password } = req.body;
+
+  let hashPassword = bcrypt.hashSync(password, 10);
+
+  db.connect((err, client, done) => {
+    if (err) throw err;
+
+    let query = `INSERT INTO tb_user(name, email, password) VALUES
+                        ('${name}','${email}','${hashPassword}')`;
+
+    client.query(query, (err, result) => {
+      done();
+      if (err) throw err;
+      req.flash("success", "Account succesfully registered ");
+      res.redirect("/login");
+    });
+  });
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.post("/login", function (req, res) {
+  let { email, password } = req.body;
+
+  db.connect((err, client, done) => {
+    if (err) throw err;
+
+    let query = `SELECT * FROM tb_user WHERE email='${email}'`;
+
+    client.query(query, (err, result) => {
+      done();
+      if (err) throw err;
+
+      if (result.rows.length == 0) {
+        req.flash("danger", "Account not found!");
+        return res.redirect("/login");
+      }
+
+      let isMatch = bcrypt.compareSync(password, result.rows[0].password);
+
+      if (isMatch) {
+        req.session.isLogin = true;
+        req.session.user = {
+          id: result.rows[0].id,
+          email: result.rows[0].email,
+          name: result.rows[0].name,
+        };
+        req.flash("success", "Login Success");
+        res.redirect("/blog");
+      } else {
+        req.flash("failed", "Password doesnt match");
+        res.redirect("/login");
+      }
+    });
+  });
+});
+
+app.get("/logout", function (req, res) {
+  req.session.destroy();
+  res.redirect("/home");
 });
 
 // Konfigurasi port aplikasi
